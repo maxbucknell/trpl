bdcdf2c2990127d96e9cdd28cd7926c33fe3fe50
Max Bucknell <me@maxbucknell.com>
2023-07-16 23:24:28 +1000

    [03.1.1]: Unsigned Addition

--- CH-03/3.1.1 Integers/src/main.rs
+++ CH-03/3.1.1 Integers/src/main.rs
@@ -8,7 +8,7 @@ pub fn to(input: NativeInt) -> Int {
     let mut result: Int = [false; 8];
     let mut counter = input;
 
-    for index in (0..8).rev() {
+    for index in (0..SIZE).rev() {
         let value: NativeInt = 1 << index;
 
         if counter >= value {
@@ -27,10 +27,76 @@ pub fn to(input: NativeInt) -> Int {
 pub fn from(input: Int) -> NativeInt {
     let mut result: NativeInt = 0;
 
-    for index in 0..8 {
+    for index in 0..SIZE {
         if input[index] {
             result += 1 << index;
         }
+    }
+
+    result
+}
+
+enum BitAddResult {
+    A0,
+    A1,
+    A2,
+    A3
+}
+
+ impl BitAddResult {
+    fn from(a: bool, b: bool, carry: bool) -> Self {
+        let mut counter = 0;
+
+        if a {
+            counter += 1;
+        }
+
+        if b {
+            counter += 1;
+        }
+
+        if carry {
+            counter += 1;
+        }
+
+        match counter {
+            0 => Self::A0,
+            1 => Self::A1,
+            2 => Self::A2,
+            3 => Self::A3,
+            _ => panic!("Unexpected bit value with: {a}, {b}, {carry}.")
+        }
+    }
+}
+
+pub fn add(a: Int, b: Int) -> Int {
+    let mut result: Int = [false; 8];
+
+    for index in 0..SIZE {
+        let a = a[index];
+        let b = b[index];
+        let c = result[index];
+
+        let bit = BitAddResult::from(a, b, c);
+
+        result[index] = match bit {
+            BitAddResult::A0 => false,
+            BitAddResult::A1 => true,
+            BitAddResult::A2 => {
+                if index < SIZE {
+                    result[index + 1] = true;
+                }
+
+                false
+            },
+            BitAddResult::A3 => {
+                if index < SIZE {
+                    result[index + 1] = true;
+                }
+
+                true
+            }
+        };
     }
 
     result
@@ -55,5 +121,13 @@ mod tests {
         assert_eq!(1, from([true, false, false, false, false, false, false, false]));
         assert_eq!(127, from([true, true, true, true, true, true, true, false]));
         assert_eq!(75, from([true, true, false, true, false, false, true, false]));
+    }
+
+    #[test]
+    fn test_add() {
+        assert_eq!(from(add(to(0), to(0))), 0);
+        assert_eq!(from(add(to(1), to(1))), 2);
+        assert_eq!(from(add(to(1), to(2))), 3);
+        assert_eq!(from(add(to(31), to(32))), 63);
     }
 }
