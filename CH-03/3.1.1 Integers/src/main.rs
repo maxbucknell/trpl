f0fc7053ebb56688103d15cf78125eb2f0024129
Max Bucknell <me@maxbucknell.com>
2023-07-31 23:40:48 +1000

    [03.1.1]: Signed Addition

--- CH-03/3.1.1 Integers/src/main.rs
+++ CH-03/3.1.1 Integers/src/main.rs
@@ -1,8 +1,7 @@
 
 const SIZE: usize = 8;
-
-type NativeInt = u8;
 type Int = [bool; SIZE];
+type NativeInt = i8;
 
 pub fn to(input: NativeInt) -> Int {
     let mut result: Int = [false; 8];
@@ -11,9 +10,17 @@ pub fn to(input: NativeInt) -> Int {
     for index in (0..SIZE).rev() {
         let value: NativeInt = 1 << index;
 
-        if counter >= value {
-            counter -= value;
-            result[index] = true;
+        if index == SIZE - 1 {
+            // We are in sign bit
+            if counter < 0 {
+                counter -= value;
+                result[index] = true
+            }
+        } else {
+            if counter >= value {
+                counter -= value;
+                result[index] = true;
+            }
         }
     }
 
@@ -36,40 +43,20 @@ pub fn from(input: Int) -> NativeInt {
     result
 }
 
-enum BitAddResult {
-    A0,
-    A1,
-    A2,
-    A3
-}
-
- impl BitAddResult {
-    fn from(a: bool, b: bool, carry: bool) -> Self {
-        let mut counter = 0;
-
-        if a {
-            counter += 1;
-        }
-
-        if b {
-            counter += 1;
-        }
-
-        if carry {
-            counter += 1;
-        }
-
-        match counter {
-            0 => Self::A0,
-            1 => Self::A1,
-            2 => Self::A2,
-            3 => Self::A3,
-            _ => panic!("Unexpected bit value with: {a}, {b}, {carry}.")
+pub fn eq(a: Int, b: Int) -> bool {
+    for index in 0..SIZE {
+        if a[index] != b[index] {
+            return false;
         }
     }
+
+    true
 }
 
+
+
 pub fn add(a: Int, b: Int) -> Int {
+    // Declare the result to be 0 at first
     let mut result: Int = [false; 8];
 
     for index in 0..SIZE {
@@ -77,26 +64,42 @@ pub fn add(a: Int, b: Int) -> Int {
         let b = b[index];
         let c = result[index];
 
-        let bit = BitAddResult::from(a, b, c);
+        let mut d: usize = 0;
 
-        result[index] = match bit {
-            BitAddResult::A0 => false,
-            BitAddResult::A1 => true,
-            BitAddResult::A2 => {
-                if index < SIZE {
-                    result[index + 1] = true;
-                }
-
-                false
-            },
-            BitAddResult::A3 => {
-                if index < SIZE {
-                    result[index + 1] = true;
-                }
-
-                true
+        // There are three possibilities when calculating the sum of
+        // bits a + b:
+        //  - 0: Both bits 0,
+        //  - 1: One bit 1, one 0,
+        //  - 2: Both bits 1.
+        //
+        // If both bits are 1, then the binary representation is 0b10,
+        // meaning that we have to carry a bit over to the next loop.
+        // The carried bit will be stored at the current position in
+        // the result. This is safe because we haven't set that value
+        // yet, that's what this loop iteration is for!
+        //
+        // This gives us a fourth possibility:
+        //  - 3: Both bits 1, and something carried from the previous
+        //    bit position.
+        //
+        // We can work out which of these scenarios we are in by just
+        // counting how many of a, b, and c (our carried bit) are true.
+        for x in [a, b, c] {
+            if x {
+                d += 1;
             }
-        };
+        }
+
+        // If the value is greater than 1, we need to carry the 1 over
+        // to the next bit. We do a basic check here to see that we do
+        // not overflow. In the case that an addition would take us out
+        // of bounds, we simply ignore it.
+        if d > 1 && (index + 1) < SIZE {
+            result[index + 1] = true;
+        }
+
+        // Odd is 1, even is 0
+        result[index] = d % 2 == 1;
     }
 
     result
@@ -114,7 +117,6 @@ mod tests {
         assert_eq!(to(75), [true, true, false, true, false, false, true, false]);
     }
 
-
     #[test]
     fn test_from() {
         assert_eq!(0, from([false; 8]));
@@ -124,10 +126,36 @@ mod tests {
     }
 
     #[test]
+    fn test_eq() {
+        assert!(eq(to(11), to(11)));
+        assert!(!eq(to(124), to(17)));
+    }
+
+    #[test]
     fn test_add() {
         assert_eq!(from(add(to(0), to(0))), 0);
         assert_eq!(from(add(to(1), to(1))), 2);
         assert_eq!(from(add(to(1), to(2))), 3);
         assert_eq!(from(add(to(31), to(32))), 63);
+    }
+
+    #[test]
+    fn test_to_signed() {
+        assert_eq!(to(-1), [true; SIZE]);
+        assert_eq!(to(-128), [false, false, false, false, false, false, false, true]);
+        assert_eq!(to(-19), [true, false, true, true, false, true, true, true]);
+    }
+
+    #[test]
+    fn test_from_signed() {
+        assert_eq!(-1, from([true; SIZE]));
+        assert_eq!(-128, from([false, false, false, false, false, false, false, true]));
+        assert_eq!(-19, from([true, false, true, true, false, true, true, true]));
+    }
+
+    #[test]
+    fn test_add_signed() {
+        assert_eq!(from(add(to(127), to(-127))), 0);
+        assert_eq!(from(add(to(75), to(-19))), 56);
     }
 }
